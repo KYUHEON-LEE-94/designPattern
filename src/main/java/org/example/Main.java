fef85c70196b79a8af30e1bd078e7b60ec2a26ae
khlee <panpan68@naver.com>
2024-02-20 15:07:43 +0900

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;/**
+ *packageName    : org.example
+ * fileName       : ${NAME}
+ * author         : heon
+ * date           : ${DATE}
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * ${DATE}           heon               최초 생성
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
